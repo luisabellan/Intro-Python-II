f9f65099e2cca02e75236254a88538cd2253ed6a
luisabellan <luis.abellan.dev@gmail.com>
2020-06-10 16:40:04 +0200

    bugged fixed

--- src/player.py
+++ src/player.py
@@ -2,3 +2,8 @@
 # currently.
 class Player:
     def __init__(self, name, current_room):
+        self.name = name
+        self.current_room = current_room
+
+    def __str__(self):
+        return (f"Name: {self.name}\n Current room: {self.current_room}\n")
--- src/room.py
+++ src/room.py
@@ -1,5 +1,10 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
 #
-class Player:
-    def __init__(self, name, current_room):
+class Room:
+    def __init__(self, name, description):
+        self.name = name
+        self.description = description
+
+    def __str__(self):
+        return (f"Name: {self.name}\n Description: {self.description}\n")
